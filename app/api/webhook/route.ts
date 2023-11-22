8f6a7dc34ad15e0b7340d2636f5ca7b95cb31fe7
deniafe~ <detoyin@yahoo.com>
2023-11-22 13:23:25 +0100

    Change webhook from the GET to POST

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import { headers } from "next/headers";
 import { admin } from "@/lib/firebase";
 
-export async function GET(req: Request) {
+export async function POST(req: Request) {
   const db = admin.firestore();
   const dataRef = db.collection('users');
 
